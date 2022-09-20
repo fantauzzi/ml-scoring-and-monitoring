a3f4d61faf21932f55d3c70e7e4d3d7d91d14256
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2022-09-20 08:32:55 +0200

    Implemented data ingestion

--- ingestion.py
+++ ingestion.py
@@ -1,26 +1,33 @@
 import pandas as pd
+import csv
 import numpy as np
 import os
 import json
+from pathlib import Path
 from datetime import datetime
 
 
-
-
-#############Load config.json and get input and output paths
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-input_folder_path = config['input_folder_path']
-output_folder_path = config['output_folder_path']
-
-
-
 #############Function for data ingestion
-def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+def merge_multiple_dataframe(input_folder_path, output_folder_path):
+    # check for datasets, compile them together, and write to an output file
+    input_files = Path(input_folder_path).glob('*.csv')
+    input_files = [str(file_name) for file_name in input_files if file_name.is_file()]
+    df = None
+    for file_name in input_files:
+        df_from_file = pd.read_csv(file_name)
+        df = df_from_file.copy() if df is None else pd.concat([df, df_from_file], ignore_index=True)
+    df = df.drop_duplicates()
+    df.to_csv(f'{output_folder_path}/finaldata.csv', index=False)
 
+    with open(f'{output_folder_path}/ingestedfiles.txt', 'w') as f:
+        csv_writer = csv.writer(f, delimiter=',')
+        csv_writer.writerow(input_files)
 
 
 if __name__ == '__main__':
-    merge_multiple_dataframe()
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+
+    input_folder_path = config['input_folder_path']
+    output_folder_path = config['output_folder_path']
+    merge_multiple_dataframe(input_folder_path, output_folder_path)
